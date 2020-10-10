156a3dffb693102a4fd7efbdb60e13abbd7f0f23
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-10 20:40:54 +0300

    theme colors

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#3B125F",
+        secondary: "#8B5FBF",
+        accent: "#BF653F",
+        error: "#722530",
+        warning: "#A37513",
+        info: "#396893",
+        success: "#4caf50"
+      }
+    }
+  },
 });
