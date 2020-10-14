21470daddf32eddab6bee8ffd0bd60eba72c0316
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-14 23:44:23 +0300

    signin form and signin request response log

--- client/src/components/Auth/Signin.vue
+++ client/src/components/Auth/Signin.vue
@@ -1,11 +1,72 @@
 <template>
-  <v-container>
-    <h1>Signin</h1>
+  <v-container mt-5 pt-5>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text-center">Welcome Back!</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card color="secondary" dark>
+          <v-container>
+            <v-form @submit.prevent="handleSigninUser">
+              <v-layout row>
+                <v-flex xs12 mr-3 ml-3>
+                  <v-text-field
+                    v-model="username"
+                    prepend-icon="mdi-face"
+                    label="Username"
+                    type="text"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+
+              <v-layout row>
+                <v-flex xs12 mr-3 ml-3>
+                  <v-text-field
+                    v-model="password"
+                    prepend-icon="mdi-form-textbox-password"
+                    label="Password"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+
+              <v-layout align-content-center column wrap>
+                <div class="text-center">
+                  <v-btn color="accent" type="submit">Signin</v-btn>
+                </div>
+                <h3>
+                  Don't have an account?
+                  <router-link to="/signup">Signup</router-link>
+                </h3>
+              </v-layout>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'Signin',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    handleSigninUser() {
+      this.$store.dispatch('signinUser', {
+        username: this.username,
+        password: this.password,
+      });
+    },
+  },
 };
 </script>
