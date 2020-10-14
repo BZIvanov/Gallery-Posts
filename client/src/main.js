bf965e3f4d52f23697de915a58bb9d4ca401a6a3
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-14 23:55:31 +0300

    apollo setup to send the token as authorization header

--- client/src/main.js
+++ client/src/main.js
@@ -11,6 +11,31 @@ Vue.use(VueApollo);
 
 export const defaultClient = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  request: (operation) => {
+    if (!localStorage.token) {
+      localStorage.setItem('token', '');
+    }
+
+    operation.setContext({
+      headers: {
+        authorization: localStorage.getItem('token'),
+      },
+    });
+  },
+  onError: ({ graphQLErrors, networkError }) => {
+    if (networkError) {
+      console.log('[networkError]', networkError);
+    }
+
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        console.dir(err);
+      }
+    }
+  },
 });
 
 const apolloProvider = new VueApollo({ defaultClient });
